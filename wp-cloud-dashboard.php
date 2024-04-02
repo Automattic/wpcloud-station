19113050b1c99e57becf04496a3a3888ee9e08ee
jhnstn <jhnstn@pm.me>
2024-04-02 13:26:33 -0400

    Add initial plugin scaffold

---
+++ wp-cloud-dashboard.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wp Cloud Dashboard
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-cloud-dashboard
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Cloud_Dashboard
+ */
+
+// Your code starts here.
