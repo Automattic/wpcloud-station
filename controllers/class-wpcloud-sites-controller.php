28358e70a3509aa3edd0563e91450e449c21c911
Austin Winstanley <austin.winstanley@automattic.com>
2024-04-04 13:11:40 +0000

    Add initial sites REST controller

---
+++ controllers/class-wpcloud-sites-controller.php
@@ -0,0 +1,103 @@
+<?php
+
+if ( ! class_exists( 'WPCLOUD_Sites_Controller' ) ) {
+
+	class WPCLOUD_Sites_Controller extends WP_REST_Controller {
+
+		/**
+		 * The namespace.
+		 *
+		 * @var string
+		 */
+		protected $namespace = 'wpcloud/v1';
+		/**
+		 * Rest base for the current object.
+		 *
+		 * @var string
+		 */
+		protected $rest_base = 'sites';
+
+		/**
+		 * Register the routes.
+		 */
+		public function register_routes() {
+			register_rest_route(
+				$this->namespace, '/' . $this->rest_base,
+				array(
+					array(
+						'methods'             => WP_REST_Server::READABLE,
+						'callback'            => array( $this, 'get_sites' ),
+						'permission_callback' => 'is_user_logged_in',
+					),
+					array(
+						'methods'             => WP_REST_Server::CREATABLE,
+						'callback'            => array( $this, 'create_site' ),
+						'permission_callback' => 'is_user_logged_in',
+					),
+				))
+			;
+
+			register_rest_route(
+				$this->namespace, '/' . $this->rest_base . '/(?P<id>[\d]+)',
+				array(
+					array(
+						'methods'             => WP_REST_Server::READABLE,
+						'callback'            => array( $this, 'get_site' ),
+						'permission_callback' => 'is_user_logged_in',
+					),
+				)
+			);
+		}
+
+		/**
+		 * Get a collection of sites.
+		 *
+		 * @param WP_REST_Request $request
+		 *
+		 * @return WP_Error|WP_REST_Response
+		 */
+		public function get_sites( $request ) {
+			require_once  plugin_dir_path( __FILE__ ) . '../wpcloud-client.php';
+
+			$sites = wpcloud_client_site_list( 'wp_version', 'php_version', 'space_quota', 'db_file_size', 'static_file_404', 'suspended' );
+			if ( is_wp_error( $sites) ) {
+				return $sites;
+			}
+
+			return new WP_REST_Response( $sites, WP_Http::OK );
+		}
+
+		/**
+		 * Get site details.
+		 *
+		 * @param WP_REST_Request $request
+		 *
+		 * @return WP_Error|WP_REST_Response
+		 */
+		public function get_site( $request ) {
+			require_once  plugin_dir_path( __FILE__ ) . '../wpcloud-client.php';
+
+			$params = $request->get_params();
+			$atomic_site_id = intval( $params['id'] );
+
+			$site_details = wpcloud_client_site_details( $atomic_site_id );
+			if ( is_wp_error( $site_details ) ) {
+				return $site_details;
+			}
+
+			return new WP_REST_Response( $site_details, WP_Http::OK );
+		}
+
+		/**
+		 * Create a new site.
+		 *
+		 * @param WP_REST_Request $request
+		 *
+		 * @return WP_Error|WP_REST_Response
+		 */
+		public function create_site( $request ) {
+			return new WP_REST_Response( null, WP_Http::NOT_IMPLEMENTED );
+		}
+	}
+
+}
