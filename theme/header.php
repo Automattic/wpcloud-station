4afd8e072e1e214ba9e0436b5518a9d68a342182
Jason Johnston <jhnstn@users.noreply.github.com>
2024-04-09 14:39:30 -0400

    First pass as layout with sidebar (#30)

---
+++ theme/header.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Header file for the WP Cloud Dashboard theme.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WP Cloud
+ * @subpackage WP_Cloud_Dashboard
+ */
+
+?><!DOCTYPE html>
+
+<html class="no-js" <?php language_attributes(); ?>>
+
+	<head>
+
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+
+		<link rel="profile" href="https://gmpg.org/xfn/11">
+		<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
+
+		<?php wp_head(); ?>
+
+	</head>
+
+	<body <?php body_class(); ?> >
+		<?php wp_body_open(); ?>
+		<div class="layout">
+		 <?php get_sidebar(); ?>
---
+++ theme/page.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying single posts and pages.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WP Cloud
+ * @subpackage WP_Cloud_Dashboard
+ */
+get_header();
+?>
+
+<main id="site-content">
+
+	<?php
+
+	if ( have_posts() ) {
+
+		while ( have_posts() ) {
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );
+		}
+	}
+
+	?>
+
+</main><!-- #site-content -->
+
+<?php
+get_footer();
