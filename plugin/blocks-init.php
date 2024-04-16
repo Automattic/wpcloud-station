62fae6650f7b58fe679f2a98e3375028252d667c
jhnstn <jhnstn@pm.me>
2024-04-16 17:35:33 -0400

    update prefix

--- plugin/blocks-init.php
+++ plugin/blocks-init.php
@@ -17,12 +17,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-function wpcdb_include_blocks() {
+function wpcloud_include_blocks() {
 	foreach( glob( __DIR__ . '/blocks/src/*/index.php' ) as $block ) {
 		require_once $block;
 	}
 }
 
-add_action( 'init', 'wpcdb_include_blocks', 9 );
+add_action( 'init', 'wpcloud_include_blocks', 9 );
 
 //require_once __DIR__ . '/blocks/src/form/index.php';
