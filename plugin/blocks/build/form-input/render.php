36129612f12338220ad57695d22d2a8c8ce6aed1
jhnstn <jhnstn@pm.me>
2024-04-17 19:06:17 -0400

    Add ability to filter form fields when rendering

---
+++ plugin/blocks/build/form-input/render.php
@@ -0,0 +1,20 @@
+<?php
+
+$limit_to_admin = $attributes['adminOnly'] ?? false;
+
+if ( $limit_to_admin && ! current_user_can( 'manage_options' ) ) {
+	return '';
+}
+
+$name = $attributes['name'] ?? 'any';
+
+$allowed = apply_filters( 'wpcloud_block_form_allow_field_' . $name,  true, $attributes, $block );
+
+if ( ! $allowed ) {
+	return '';
+}
+
+$content = apply_filters( 'wpcloud_block_form_render_field_' . $name, $content, $attributes, $block );
+
+
+echo $content;
