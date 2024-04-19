3184205f2421985fd51f55e2b4305da4b0cd4f90
jhnstn <jhnstn@pm.me>
2024-04-18 21:58:34 -0400

    Add site detail card

---
+++ plugin/blocks/build/site-detail-card/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'd457dbaf6719d4ab3d2a');
---
+++ plugin/blocks/src/site-detail-card/edit.js
@@ -0,0 +1,79 @@
+/**
+ * External dependencies
+ */
+import classNames from 'classnames';
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import {
+	InnerBlocks,
+	useBlockProps,
+	useInnerBlocksProps,
+	InspectorControls,
+} from '@wordpress/block-editor';
+import { CheckboxControl, PanelBody } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import './editor.scss';
+
+/**
+ *
+ * @param {Object}  props               Component props.
+ * @param {Object}  props.attributes
+ * @param {Object}  props.setAttributes
+ * @param {number}  props.clientId
+ * @param {boolean} props.isSelected
+ * @return {Element} Element to render.
+ */
+export default function Edit( {
+	attributes,
+	setAttributes,
+	clientId,
+	isSelected,
+} ) {
+	const { adminOnly } = attributes;
+	const blockProps = useBlockProps();
+
+	const isChildSelected = useSelect( ( select ) =>
+		select( 'core/block-editor' ).hasSelectedInnerBlock( clientId )
+	);
+
+	const innerBlocksProps = useInnerBlocksProps( blockProps, {
+		renderAppender:
+			isSelected || isChildSelected
+				? InnerBlocks.ButtonBlockAppender
+				: undefined,
+	} );
+
+	return (
+		<>
+			<InspectorControls>
+				<PanelBody title={ __( 'Form Settings' ) }>
+					<CheckboxControl
+						label={ __( 'Limit to Admins' ) }
+						checked={ adminOnly }
+						onChange={ ( newVal ) => {
+							setAttributes( {
+								adminOnly: newVal,
+							} );
+						} }
+						help={ __(
+							'Only admins will see this field. Inputs marked as admin only will appear with a dashed border in the editor'
+						) }
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<div
+				{ ...innerBlocksProps }
+				className={ classNames( 'wpcloud-block-site-detail-card', {
+					'is-admin-only': adminOnly,
+				} ) }
+			/>
+		</>
+	);
+}
