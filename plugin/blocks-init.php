1ee3043b78748a8826cca51ab9ca42c2866cce26
jhnstn <jhnstn@pm.me>
2024-04-18 20:30:06 -0400

    Change order of when the index.php file gets loaded
    This way the block is registered before index.php loads

--- plugin/blocks-init.php
+++ plugin/blocks-init.php
@@ -20,21 +20,25 @@ if ( ! defined( 'ABSPATH' ) ) {
 function wpcloud_include_blocks() {
 
 	foreach( glob( __DIR__ . '/blocks/src/*' ) as $block_directory ) {
+		try {
+			register_block_type(
+				dirname( __FILE__ ) . '/blocks/build/' . basename( $block_directory )
+			);
+		} catch ( Exception $e ) {
+			error_log( 'Error registering block: ' . $e->getMessage() );
+		}
+
 		if ( is_file( $block_directory . '/index.php' ) ) {
 			// If we need any server code, include a blocks/src/{block}/index.php file.
 			require_once $block_directory . '/index.php';
 		}
-
-		register_block_type(
-			dirname( __FILE__ ) . '/blocks/build/' . basename( $block_directory )
-		);
 	}
 }
 add_action( 'init', 'wpcloud_include_blocks' );
 
 add_filter( 'block_categories_all' , function( $categories ) {
 
-    // Adding a new category.
+  // Adding a new category.
 	$categories[] = array(
 		'slug'  => 'wpcloud',
 		'title' => 'WP Cloud'
