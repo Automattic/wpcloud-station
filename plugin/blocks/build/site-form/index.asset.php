301694626410fc4a095c1e58c7dd0c14b9f055ff
jhnstn <jhnstn@pm.me>
2024-04-22 16:29:25 -0400

    build update

--- plugin/blocks/build/site-form/index.asset.php
+++ plugin/blocks/build/site-form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e6bac04d58b5faea0c65');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '822fcffd524e23054e42');
