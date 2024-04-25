af53626a0bf7cd61f7f8f6f48edec3b1b9eacb9e
jhnstn <jhnstn@pm.me>
2024-04-25 14:45:28 -0400

    update style and handle make primary button events

--- plugin/blocks/src/site-alias/view.js
+++ plugin/blocks/src/site-alias/view.js
@@ -3,8 +3,24 @@
 
 	const aliasList = document.querySelector('.wpcloud-block-site-alias-list');
 	const removeForm = document.querySelector('.wpcloud-block-site-alias-form-remove');
-
+	const makePrimaryButtons = document.querySelectorAll('.wpcloud-block-site-alias-make-primary');
 	const newAliasInput = document.querySelector('.wpcloud-block-site-alias-form-add input[name=site_alias]');
+
+
+	function bindMakePrimaryButton(button) {
+		if (!button) {
+			return;
+		}
+		button.addEventListener('click', (e) => {
+			e.preventDefault();
+			const form = e.target.closest('form');
+			form.querySelector('input[name=wpcloud_action]').value = 'site_alias_make_primary';
+			console.log(form);
+			form.dispatchEvent(new Event('submit'));
+		});
+	}
+
+	makePrimaryButtons.forEach( bindMakePrimaryButton );
 
 	wpcloud.hooks.addAction('wpcloud_form_response', 'site_alias_add', (result) => {
 		if (result?.action === 'site_alias_add') {
@@ -15,7 +31,7 @@
 				newAlias.dataset.siteAlias = result.site_alias;
 				newAlias.removeAttribute('style');
 				aliasList.appendChild(newAlias);
-
+				bindMakePrimaryButton( newAlias.querySelector('.wpcloud-block-site-alias-make-primary'));
 				wpcloud.bindFormHandler(newAlias);
 				newAliasInput.value = '';
 
