7f794fb67c73a9a1f71c96d0d2b65589c5545441
jhnstn <jhnstn@pm.me>
2024-04-30 16:38:54 -0400

    hook in delete site

--- plugin/blocks/src/site-delete/index.php
+++ plugin/blocks/src/site-delete/index.php
@@ -1,9 +1,26 @@
 <?php
 
 
-function wpcloud_block_form_site_delete_handler( $response, $data ) {
-	$response['success'] = false;
-	$response['status'] = 501;
+function wpcloud_block_form_site_delete_handler( $response, $data) {
+	$wpcloud_site_id = get_post_meta( $data['site_id'], 'wpcloud_site_id', true );
+
+	if ( ! $wpcloud_site_id ) {
+		$response['message'] = 'Site not found.';
+		$response['status'] = 400;
+		return $response;
+	}
+
+	$result = wpcloud_client_site_delete( $wpcloud_site_id );
+
+	if ( is_wp_error( $result ) ) {
+		$response['success'] = false;
+		$response['message'] = $result->get_error_message();
+		$response['status'] = 400;
+		return $response;
+	}
+
+	$response['message'] = 'Site deleted successfully.';
+	$response['redirect'] = '/sites';
 
 	return $response;
 }
