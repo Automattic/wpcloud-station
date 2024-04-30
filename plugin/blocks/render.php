a355e4ac3f666bb8f2d464201d8bf29af75c3d11
jhnstn <jhnstn@pm.me>
2024-04-30 14:52:05 -0400

    move form helpers to blocks/render

--- plugin/blocks/render.php
+++ plugin/blocks/render.php
@@ -17,3 +17,22 @@ function wpcloud_block_add_hidden_site_alias_field( DOMDocument $dom, DOMNode $f
 		$domain_input->setAttribute('value', $value);
 		$form->appendChild($domain_input);
 }
+
+// Add the site id to any form that is on a site post.
+function wpcloud_block_add_site_id_hidden_field( $content ) {
+	if ( get_post_type() !== 'wpcloud_site' ) {
+		return $content;
+	}
+	$site_id = get_the_ID();
+	return wpcloud_block_form_hidden_field( 'site_id', $site_id ) . $content;
+}
+add_filter( 'wpcloud_block_form_fields', 'wpcloud_block_add_site_id_hidden_field' );
+
+// Allow the site id in the form if present.
+function wpcloud_block_allow_site_id_field( $fields, $post_keys ) {
+	if ( in_array( 'site_id', $post_keys ) ) {
+		return array_merge( $fields, [ 'site_id' ] );
+	}
+	return $fields;
+}
+add_filter( 'wpcloud_block_form_submitted_fields', 'wpcloud_block_allow_site_id_field', 10, 2 );
