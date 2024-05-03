ffd3535e721f008709cc4921426bfcf7b0ba997e
jhnstn <jhnstn@pm.me>
2024-05-03 12:57:22 -0400

    Delete the post, not the site

--- plugin/blocks/src/site-delete/index.php
+++ plugin/blocks/src/site-delete/index.php
@@ -2,19 +2,19 @@
 
 
 function wpcloud_block_form_site_delete_handler( $response, $data) {
-	$wpcloud_site_id = get_post_meta( $data['site_id'], 'wpcloud_site_id', true );
-
-	if ( ! $wpcloud_site_id ) {
+	//$wpcloud_site_id = get_post_meta( $data['site_id'], 'wpcloud_site_id', true );
+	$post = get_post( $data['site_id'] );
+	if ( ! $post) {
 		$response['message'] = 'Site not found.';
 		$response['status'] = 400;
 		return $response;
 	}
 
-	$result = wpcloud_client_site_delete( $wpcloud_site_id );
+	$result = wp_delete_post( $data['site_id'], true );
 
 	if ( is_wp_error( $result ) ) {
 		$response['success'] = false;
-		$response['message'] = $result->get_error_message();
+		$response['message'] = 'Error deleting site.';
 		$response['status'] = 400;
 		return $response;
 	}
