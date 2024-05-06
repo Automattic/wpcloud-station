296b1c8c4a5976e36141ec0e496ede673ab503b9
jhnstn <jhnstn@pm.me>
2024-05-06 17:32:14 -0400

    update build

---
+++ plugin/blocks/build/components/link/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd295a6897f4f0f84969e');
--- plugin/blocks/src/site-list/index.js
+++ plugin/blocks/src/site-list/index.js
@@ -10,68 +10,78 @@ import './style.scss';
 import metadata from './block.json';
 
 registerBlockVariation( 'core/query', {
-    name: metadata.name,
-    title: 'Site List',
-    description: 'Displays a list of WP Cloud Sites',
-    isActive: ( { namespace, query } ) => {
-        return (
-            namespace === metadata.name
-            && query.postType === 'wpcloud_site'
-        );
-    },
-    attributes: {
-        namespace: metadata.name,
-        query: {
-            perPage: 10,
-            pages: 0,
-            offset: 0,
-            postType: 'wpcloud_site',
-            order: 'desc',
-            orderBy: 'date',
-            author: '',
-            search: '',
-            exclude: [],
-            sticky: '',
-            inherit: false,
-            status: 'any',
-        },
-    },
-    scope: [ 'inserter' ],
-    innerBlocks: [
-        [
-            'core/post-template',
-            {},
-            [
-                ['wpcloud/site-card', {}],
-                ['core/post-author', {
-                    showAvatar: false,
-                    isLink: true,
-                    showBio: false,
-                    textAlign: 'center',
-                }],
-                ['core/post-date', {
-                    textAlign: 'center',
-                }],
-                ['wpcloud/site-detail', {
-                    name: 'php_version',
-                    label: 'PHP',
-                    hideLabel: true,
-                }],
-                ['wpcloud/site-detail', {
-                    name: 'wp_version',
-                    label: 'WP',
-                    hideLabel: true,
-                }],
-                ['wpcloud/site-detail', {
-                    name: 'ip_addresses',
-                    label: 'IP Address',
-                    hideLabel: true,
-                }],
-
-            ],
+	name: metadata.name,
+	title: 'Site List',
+	description: 'Displays a list of WP Cloud Sites',
+	isActive: ( { namespace, query } ) => {
+		return namespace === metadata.name && query.postType === 'wpcloud_site';
+	},
+	attributes: {
+		namespace: metadata.name,
+		query: {
+			perPage: 10,
+			pages: 0,
+			offset: 0,
+			postType: 'wpcloud_site',
+			order: 'desc',
+			orderBy: 'date',
+			author: '',
+			search: '',
+			exclude: [],
+			sticky: '',
+			inherit: false,
+			status: 'any',
+		},
+	},
+	scope: [ 'inserter' ],
+	innerBlocks: [
+		[
+			'core/post-template',
+			{},
+			[
+				[ 'wpcloud/site-card', {} ],
+				[
+					'core/post-author',
+					{
+						showAvatar: false,
+						isLink: true,
+						showBio: false,
+						textAlign: 'center',
+					},
+				],
+				[
+					'core/post-date',
+					{
+						textAlign: 'center',
+					},
+				],
+				[
+					'wpcloud/site-detail',
+					{
+						name: 'php_version',
+						label: 'PHP',
+						hideLabel: true,
+					},
+				],
+				[
+					'wpcloud/site-detail',
+					{
+						name: 'wp_version',
+						label: 'WP',
+						hideLabel: true,
+					},
+				],
+				[
+					'wpcloud/site-detail',
+					{
+						name: 'ip_addresses',
+						label: 'IP Address',
+						hideLabel: true,
+					},
+				],
+			],
 		],
 		[ 'core/query-pagination' ],
 		[ 'core/query-no-results' ],
 	],
-    }
-);
+} );
