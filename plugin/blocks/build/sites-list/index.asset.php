188f93f51ee6c288f2bcb2ba04cdeab9fefed46b
jhnstn <jhnstn@pm.me>
2024-05-06 15:25:05 -0400

    remove static sites list

--- plugin/blocks/build/sites-list/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd5d5bc82b97ca82bae38');
--- plugin/blocks/build/sites-list/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'e8ec0ef4aa337cceda4f');
