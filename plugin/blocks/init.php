5daf4e1e41325d0dd58edd453f9ce8baa5ace235
Jason Johnston <jhnstn@users.noreply.github.com>
2024-05-15 16:25:52 -0400

    Fix/plugin header (#94)
    * exclude non production files from the plugin zip
    
    * use distignore when building the release asset
    
    * add block src to distignore
    
    * add more to dist ignore
    
    * move conditional require up with other requires
    
    * remove bogus header comment

--- plugin/blocks/init.php
+++ plugin/blocks/init.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * Plugin Name:       Example Static
- * Description:       Example block scaffolded with Create Block tool.
- * Requires at least: 6.1
- * Requires PHP:      7.0
- * Version:           0.1.0
- * Author:            The WordPress Contributors
- * License:           GPL-2.0-or-later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       example-static
- *
- * @package CreateBlock
- */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
