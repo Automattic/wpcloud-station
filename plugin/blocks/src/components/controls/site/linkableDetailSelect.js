6c688937a0c596cdac9cffb4b852fa1a376aceec
jhnstn <jhnstn@pm.me>
2024-05-21 12:37:49 -0400

    update linkable details; render from PHP

--- plugin/blocks/src/components/controls/site/linkableDetailSelect.js
+++ plugin/blocks/src/components/controls/site/linkableDetailSelect.js
@@ -4,20 +4,6 @@
 import { __ } from '@wordpress/i18n';
 import { SelectControl } from '@wordpress/components';
 
-/*
-export const formatDisplayName = ( name = '' ) => {
-	const words = name.replace( /–|-|_/g, ' ' );
-	if ( ! words.includes( ' ' ) ) {
-		return name;
-	}
-
-	return words
-		.replace( /\b\w/g, ( s ) => s.toUpperCase() )
-		.replace( /\b(.{2})\b/i, ( twoChar ) => twoChar.toUpperCase() )
-		.replace( /\bapi\b/i, 'API' )
-		.replace( /\bphp\b/i, 'PHP' );
-};
-*/
 export default function LinkableDetailSelect( {
 	attributes,
 	setAttributes,
@@ -34,10 +20,7 @@ export default function LinkableDetailSelect( {
 		<SelectControl
 			label={ __( 'Select a site detail link' ) }
 			value={ name }
-			options={ options.map( ( detailKey ) => ( {
-				value: detailKey,
-				label: formatDisplayName( detailKey ),
-			} ) ) }
+			options={ options }
 			onChange={ ( newName ) => {
 				setAttributes( {
 					name: newName,
