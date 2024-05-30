aee6301927d88f1182a54dbaae632b95401cf9ec
jhnstn <jhnstn@pm.me>
2024-05-29 21:24:12 -0400

    switch header to toggle is-open class but look for hide-on-open to animate

--- plugin/blocks/src/components/expanding-header/view.js
+++ plugin/blocks/src/components/expanding-header/view.js
@@ -2,9 +2,7 @@
 	wpcloud.hooks.addAction('wpcloud_expanding_section_toggle', 'wpcloud', (target) => {
 		const section = target.closest('.wpcloud-block-expanding-section');
 		const wrapper = section.querySelector('.wpcloud-block-expanding-section__header-wrapper');
-		if (wrapper.classList.contains('hide-on-open')) {
-			wrapper.classList.toggle('is-open');
-		}
+		wrapper.classList.toggle('is-open');
 	});
 
 })( window.wpcloud );
