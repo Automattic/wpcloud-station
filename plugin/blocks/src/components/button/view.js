da80494cfc846b6bb93c5ab7fa9993e84bb86322
Jason Johnston <jhnstn@users.noreply.github.com>
2024-05-30 22:53:10 -0400

    add back the refresh link logic (#124)

--- plugin/blocks/src/components/button/view.js
+++ plugin/blocks/src/components/button/view.js
@@ -19,4 +19,61 @@
 		}
 	});
 
+	async function refreshHref( e ) {
+		const link = e.currentTarget;
+		const button = link.closest( '.wp-block-wpcloud-button[data-refresh-rate]' );
+		const refreshRate = link.dataset.refreshRate || 15000;
+		link.dataset.refresh = '';
+		setTimeout( () => {
+			link.dataset.refresh = 'true';
+		}, refreshRate );
+
+		const formData = new FormData();
+		formData.append( 'action', 'wpcloud_refresh_link' );
+		formData.append( 'site_id', button.dataset.siteId );
+		formData.append( '_wpnonce', button.dataset.nonce );
+		formData.append( 'site_detail', button.dataset.wpcloudDetail );
+		const response = await fetch(
+			'http://localhost:8888/wp-admin/admin-ajax.php',
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+				body: new URLSearchParams( formData ).toString(),
+			}
+		);
+
+		const result = await response.json();
+		if ( ! result.success ) {
+			// @TODO: Show error message
+			return false;
+		}
+
+		link.href = result.data.url;
+	}
+
+ document
+		.querySelectorAll( '.wp-block-wpcloud-button[data-refresh-rate] a' )
+		.forEach( async ( link ) => {
+			const refreshRate = link.dataset.refreshRate || 15000;
+			setTimeout( () => {
+				link.dataset.refresh = 'true';
+			}, refreshRate );
+			link.addEventListener( 'click', async ( e ) => {
+				if ( link.dataset.refresh ) {
+					e.preventDefault();
+					await refreshHref( e );
+					link.click();
+				}
+				return true;
+			} );
+
+			link.addEventListener( 'contextmenu', async ( e ) => {
+				if ( link.dataset.refresh ) {
+					await refreshHref( e );
+				}
+			} );
+		} );
+
 })(window.wpcloud);
