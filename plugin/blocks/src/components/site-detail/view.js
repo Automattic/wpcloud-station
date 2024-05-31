efea107435d213e868d847e2a67397adb0590ea3
jhnstn <jhnstn@pm.me>
2024-05-30 22:35:55 -0400

    remove refresh from site detail and add obscure value
    the refresh flow is now available in the wpcloud/button

--- plugin/blocks/src/components/site-detail/view.js
+++ plugin/blocks/src/components/site-detail/view.js
@@ -1,6 +1,10 @@
 ( ( wpcloud ) => {
 	const onCopyToClipboard = async ( e ) => {
-		const detail = e.currentTarget.closest( '.wpcloud-block-site-detail' );
+		const detail = e.currentTarget.closest('.wpcloud-block-site-detail');
+		const isObscured = detail.querySelector('.is-obscured');
+		if (isObscured) {
+			return true;
+		}
 		const value = detail.querySelector(
 			'.wpcloud-block-site-detail__value'
 		);
@@ -28,65 +32,23 @@
 	// for dynamically created elements
 	wpcloud.copyToClipboard = wpcloud?.onCopyToClipboard || onCopyToClipboard;
 
-	document
-		.querySelectorAll( '.wpcloud-block-site-detail .copy-to-clipboard' )
-		.forEach( ( element ) => {
-			element.addEventListener( 'click', wpcloud.copyToClipboard );
-		} );
+	const siteDetails = document.querySelectorAll( '.wpcloud-block-site-detail' );
 
-	async function refreshHref( e ) {
-		const link = e.currentTarget;
-		const refreshRate = link.dataset.refreshRate || 15000;
-		link.dataset.refresh = '';
-		setTimeout( () => {
-			link.dataset.refresh = 'true';
-		}, refreshRate );
+	siteDetails.forEach((siteDetail) => {
+		siteDetail.querySelectorAll('.wpcloud-copy-to-clipboard')
+			.forEach((element) => {
+				element.addEventListener('click', wpcloud.copyToClipboard);
+			});
+	});
 
-		const formData = new FormData();
-		formData.append( 'action', 'wpcloud_refresh_link' );
-		formData.append( 'site_id', link.dataset.siteId );
-		formData.append( '_wpnonce', link.dataset.nonce );
-		formData.append( 'site_detail', link.dataset.siteDetail );
-		const response = await fetch(
-			'http://localhost:8888/wp-admin/admin-ajax.php',
-			{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded',
-				},
-				body: new URLSearchParams( formData ).toString(),
-			}
-		);
-
-		const result = await response.json();
-		if ( ! result.success ) {
-			// @TODO: Show error message
-			return false;
-		}
-
-		link.href = result.data.url;
-	}
-
-	document
-		.querySelectorAll( '.wpcloud-block-site-detail.refresh-link a' )
-		.forEach( async ( link ) => {
-			const refreshRate = link.dataset.refreshRate || 15000;
-			setTimeout( () => {
-				link.dataset.refresh = 'true';
-			}, refreshRate );
-			link.addEventListener( 'click', async ( e ) => {
-				if ( link.dataset.refresh ) {
-					e.preventDefault();
-					await refreshHref( e );
-					link.click();
-				}
-				return true;
-			} );
-
-			link.addEventListener( 'contextmenu', async ( e ) => {
-				if ( link.dataset.refresh ) {
-					await refreshHref( e );
-				}
-			} );
-		} );
+	siteDetails.forEach((siteDetail) => {
+		siteDetail.querySelectorAll('.is-obscured').forEach((element) => {
+			const revealButton = element.querySelector('.wpcloud-reveal-value');
+			revealButton.addEventListener('click', (e) => {
+				e.preventDefault();
+				e.stopPropagation();
+				element.classList.toggle('is-obscured');
+			});
+		})
+	});
 } )( window.wpcloud );
