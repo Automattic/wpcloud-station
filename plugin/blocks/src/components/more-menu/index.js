8dbad88276a603dea03baf0fadb09ca6443887ad
jhnstn <jhnstn@pm.me>
2024-06-06 17:40:18 -0400

    more editor cleanup

--- plugin/blocks/src/components/more-menu/index.js
+++ plugin/blocks/src/components/more-menu/index.js
@@ -10,15 +10,7 @@ import edit from './edit';
 import save from './save';
 import metadata from './block.json';
 
-/**
- * Every block starts by registering a new block type definition.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
- */
 registerBlockType( metadata.name, {
-	/**
-	 * @see ./edit.js
-	 */
 	edit,
 	save,
 } );
