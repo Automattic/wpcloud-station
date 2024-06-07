12796efd733f58c250d9833eb24c9ff979f2f85b
jhnstn <jhnstn@pm.me>
2024-06-07 11:03:06 -0400

    update ssh user form to handle updates

--- plugin/blocks/src/site-ssh-users/view.js
+++ plugin/blocks/src/site-ssh-users/view.js
@@ -1,29 +1,68 @@
 ((wpcloud) => {
-		wpcloud.hooks.addAction(
+
+
+	wpcloud.hooks.addAction(
 		'wpcloud_ssh_user_update_button_click',
 		'wpcloud',
 		(button) => {
-			console.log(button);
+			const sshUser = button.closest('.wpcloud-block-ssh-user-list__row');
 
-			const sshUserName = button.closest('.wpcloud-block-ssh-user').dataset.siteSshUser;
+			const sshUserName = sshUser.dataset.siteSshUser;
 
 			const sshUserContainer = button.closest('.wpcloud-block-ssh-users');
 
 			const form = sshUserContainer.querySelector('.wpcloud-block-form');
-			console.log(sshUserName);
+			const nameInput = form.querySelector('input[name="user"]');
+			nameInput.value = sshUserName;
 
-			/*
-			1. Update the form with the SSH user name.
-			2. Change the form action to 'site_ssh_user_update'.
-			3. Change submit button label to 'Update'.
-			4. Open up the expandable section.
-			5. Scroll into into the section view.
-			*/
+			const submit = form.querySelector('button[type="submit"]');
+			submit.querySelector('.wpcloud-block-button__label').textContent = 'Update';
+
+			const wpCloudAction = form.querySelector('input[name="wpcloud_action"]');
+			wpCloudAction.value = 'wpcloud_ssh_user_update';
 
 			// Open the form section.
 			const openFormButton = sshUserContainer.querySelector('[data-wpcloud-action="wpcloud_expanding_section_toggle"]');
 			wpcloud.hooks.doAction('wpcloud_expanding_section_toggle', openFormButton);
 		}
-	)
+	);
+
+	wpcloud.hooks.addAction(
+		'wpcloud_expanding_section_toggle_end',
+		'wpcloud',
+		(isOpen, section) => {
+			if (isOpen) {
+				return;
+			}
+			const sshUserSection = section.closest('.wpcloud-ssh-user-form--expanding-section');
+			if (!sshUserSection) {
+				return;
+			}
+			clearForm(sshUserSection.querySelector('.wpcloud-block-form'));
+		}
+	);
+
+	// @TODO this is probably general enough to move to the form view.js
+	function clearForm(form) {
+		form.querySelectorAll('input').forEach((input) => {
+
+			if (input.type === 'hidden') {
+				return;
+			}
+			input.value = '';
+		});
+
+		const originalLabel = form.dataset.originalLabel;
+		if (originalLabel) {
+			const submit = form.querySelector('button[type="submit"]');
+			submit.querySelector('.wpcloud-block-button__label').textContent = originalLabel;
+		}
+
+		const originalAction = form.dataset.originalAction;
+		if ( originalAction ) {
+			const wpCloudAction = form.querySelector('input[name="wpcloud_action"]');
+			wpCloudAction.value = form.dataset.originalAction;
+		}
+	}
 
 })(window.wpcloud)
