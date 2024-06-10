6989f10a8729e4d2199429b65ef8a00017a3c5a7
jhnstn <jhnstn@pm.me>
2024-06-10 16:36:20 -0400

    update template for site update block

--- plugin/blocks/src/site-update/index.js
+++ plugin/blocks/src/site-update/index.js
@@ -1,6 +1,7 @@
 /**
  * WordPress dependencies
  */
+import { __ } from '@wordpress/i18n';
 import { registerBlockType } from '@wordpress/blocks';
 import { InnerBlocks } from '@wordpress/block-editor';
 
@@ -9,7 +10,92 @@ import { InnerBlocks } from '@wordpress/block-editor';
  */
 import metadata from './block.json';
 
+const metaOptions = window.wpcloud?.siteMetaOptions || {};
+
+function addInputTemplate(name, label) {
+	let options = metaOptions[name]?.options;
+
+	const type = metaOptions[name]?.type || 'text';
+	const input = [
+		'wpcloud/form-input',
+		{
+			type,
+			name,
+			label,
+			meta: { name: label },
+		},
+	];
+
+	if (type === 'select') {
+		let optionData = [];
+
+		if (Array.isArray(options)) {
+			options.forEach((option) => {
+				optionData.push({ value: option, label: option });
+			});
+		} else {
+			for (const [key, value] of Object.entries(options)) {
+				optionData.push({ value: key, label: value });
+			}
+		}
+		input[1].options = optionData;
+	}
+
+	const hintText = metaOptions[name]?.hint;
+	if (hintText) {
+		const hint = ['wpcloud/expanding-section',
+			{
+				metadata: {
+					name: `${name} hint`,
+				},
+				clickToToggle: true,
+				hideHeader: false,
+
+			},
+			[
+				['wpcloud/expanding-header', {}, [
+					['wpcloud/icon', { icon: 'info' }],
+				]],
+				['wpcloud/expanding-content', {}, [
+					['core/paragraph', {
+						content: hintText,
+					}],
+				]],
+			]
+		];
+		input.push([ hint ]);
+	}
+
+	console.log("input", input);
+	return input
+}
+
+const template = [
+	[ 'wpcloud/form', {
+		ajax: true,
+		wpcloudAction: 'site_update',
+		submitOnChange: true,
+	},
+		[
+			addInputTemplate( 'suspended', __( 'Suspended HTTP Status Code' ) ) ,
+			addInputTemplate( 'suspend_after', __( 'Suspend after' ) ) ,
+			addInputTemplate( 'php_version', __( 'PHP Version' ) ) ,
+			addInputTemplate( 'wp_version', __( 'WP Version' ) ) ,
+			addInputTemplate( 'do_not_delete', __( 'Do Not Delete' ) ),
+			addInputTemplate( 'space_quota', __( 'Space Quota' ) ),
+			addInputTemplate( 'photon_subsizes', __( 'Photon Subsizes' ) ),
+			addInputTemplate( 'privacy_model', __( 'Privacy Model' ) ),
+			addInputTemplate('geo_affinity', __( 'Geo Affinity' ) ),
+			addInputTemplate('static_file_404', __('Static File 404' ) ),
+			addInputTemplate('default_php_conns', __('Default PHP Connections')),
+			addInputTemplate('burst_php_conns', __( 'Burst PHP Connections' ) ),
+			addInputTemplate('php_fs_permissions', __('PHP File Permissions')),
+			addInputTemplate( 'canonicalize_aliases', __('Canonize Aliases' ) ),
+		]
+	],
+]
+
 registerBlockType( metadata.name, {
-	edit: () => (<InnerBlocks />),
+	edit: () => (<InnerBlocks template={template} />),
 	save: () => (<InnerBlocks.Content />),
 } );
--- plugin/includes/class-wpcloud-site.php
+++ plugin/includes/class-wpcloud-site.php
@@ -177,95 +177,107 @@ class WPCLOUD_Site {
 		return [
 			// db_charset and db_collate should be paired ?
 			"db_charset" => [
+				'type' => 'select',
 				'options' => [ "latin1 "=> "latin1", "utf8" => "utf8", "utf8mb4" => "utf8mb4" ],
 				'default' => 'utf8mb4',
 				'hint' => '',
 				],
 			"db_collate" => [
+				'type' => 'select',
 				'options' => [ "latin1_swedish_ci" => "latin1_swedish_ci", "utf8_general_ci" => "utf8_general_ci", "utf8mb4_unicode_ci" => "utf8mb4_unicode_ci" ],
 				'default' => 'utf8mb4_unicode_ci',
 				'hint' => '',
 				],
 			"suspended" => [
+				'type' => 'select',
 				'options' => ["404" => "404", "410" => "410", "451" => "451", "480" => "480" ],
 				'default' => '480',
 				'hint' => __('Suspends a site. The value is the HTTP 4xx status code the site will respond with. The supported statuses are "404", "410", "451", and "480".'),
 				],
 			"suspend_after" => [
-				'options' => [],
+				'type' => 'text',
+				'options' => null,
 				'default' => 0,
-				'hint' => __('Suspends a site after a specified time. The value is a unix Timestamp.'),
+				'hint' => __( 'Suspends a site after a specified time. The value is a unix Timestamp.' ),
 				],
 			"php_version" => [
+				'type' => 'select',
 				'options' => wpcloud_client_php_versions_available(),
 				'default' => '',
 				'hint' => 'Sets the sites PHP version.',
 				],
 
 			"wp_version" => [
-				'options' =>  ["latest" => __("latest"), "previous" => __("previous"), "beta" => __("beta") ],
+				'type' => 'select',
+				'options' =>  [ "latest" => __("latest"), "previous" => __("previous"), "beta" => __("beta") ],
 				'default' => 'latest',
 				'hint' => 'Sets the sites WordPress version.',
 				],
 
 			"do_not_delete" => [
-				'options' => [ 0 => "false", 1 => "true" ],
-				'default' => 0,
+				'type' => 'checkbox',
+				'default' => false,
 				'hint' => __( 'Prevent a site from begin deleted. This can be useful in some cases. For example, you might wish to preserve a site while it is being reviewed for Terms of Service violations.'),
 				],
 
 			"space_quota" => [
-				'options' => [],
+				'type' => 'text',
 				'default' => 0,
 				'hint' => __('Sets the space quota for a site. Value must be an integer followed by a size specifier like "200G". 0 means unlimited.'),
 				],
 
 			"photon_subsizes" => [
+				'type' => 'select',
 				'options' => [ "0" => __( "disabled" ), "1" => __( "enabled" ), "deleted" => __( "deleted" )],
 				'default' => '0',
 				'hint' => __('controls whether WP skips generating intermediate image files when an image is uploaded. The platform is able to satisfy requests for intermediate image files whether or not they exist, so sites can save disk space by not creating them. When the a site web server receives a request for a non-existent intermediate image file, it proxies the request to Photon which responds with the intermediate image size. Can be set to "0" or "1" or deleted.'),
 				],
 
 			"privacy_model" => [
+				'type' => 'select',
 				'options' => [ "wp_uploads" => "WP Uploads" ],
 				'default' => 'wp_uploads',
 				'hint' => __( 'facilitates protection of site assets. May be set to "wp_uploads" to block logged-out requests for WP uploads. If set, an AT_PRIVACY_MODEL constant will be defined in the PHP environment. Use the "site-wordpress-version" endpoint to set "wp_version".' )
 				],
 
 			"geo_affinity" =>  [
+				'type' => 'select',
 				'options' => wpcloud_client_data_centers_available(),
 				'default' => '',
 				'hint' => __('Sets the sites geo affinity.'),
 				],
 
 			"static_file_404" => [
+				'type' => 'select',
 				'options' => [ "lightweight" => __( 'Lightweight' ), "wordpress" => __( 'WordPress' )],
 				'default' => 'wordpress',
 				'hint' => __( 'Set how a site responds to requests for non-existent static files. May be set to "lightweight" for simple, fast web server 404s. May be set to "wordpress" to delegate such requests to WordPress. The current default is "wordpress".' ),
 				],
 
 			"default_php_conns" => [
+				'type' => 'select',
 				'options' => range(2,10),
 				'default' => 0,
 				'hint' => __( 'may be used to either limit allowed concurrent PHP connections or to increase the default number of concurrent connections a site can use if the web server has spare PHP connections capacity. Clients may set any value for a site between 2 and 10; the platform has more leeway if needed.' ),
 				],
 
 			"burst_php_conns" => [
-				'options' => [0 => "disabled", 1 => "enabled"],
+				'type' => 'checkbox',
 				'default' => 0,
 				'hint' => __( 'Enable burst for sites with fewer than 10 default_php_conns. 0 or absent when default_php_conns < 10 means burst is disabled, 1 means burst is enabled.'),
 			],
 
 			"php_fs_permissions" => [
+				'type' => 'select',
 				'options' => [ "rw" => __( "Read/Write"), "ro" => __( "Read Only"), "loggedin" => __("Read only unless logged into WordPress") ],
 				'default' => 'rw',
 				'hint' => __( 'Sets the PHP file system permissions. May be set to `Read/Write`, `Read Only`, or `Logged in` for read only unless logged into WordPress.' ),
 				],
 
 			"canonicalize_aliases" => [
-				'options' => [ 0 => "false", 1 => "true" ],
+				'type' => 'checkbox',
 				'default' => 0,
-				'hint' => __( 'may be used to change whether a sites domain aliases redirect (default, "true") to the sites primary domain name or are served directly (when set to "false")' ),
+				'hint' => __( 'May be used to change whether a sites domain aliases redirect (default, "true") to the sites primary domain name or are served directly (when set to "false")' ),
 			],
 		];
 	}
