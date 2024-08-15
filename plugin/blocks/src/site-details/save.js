cbb05e87ded41121e2685a4ae10aa9812ce5251d
jhnstn <jhnstn@pm.me>
2024-08-15 14:34:53 -0400

    allwo styling admin only sections

--- plugin/blocks/src/site-details/save.js
+++ plugin/blocks/src/site-details/save.js
@@ -8,14 +8,18 @@ import classNames from 'classnames';
  */
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
-export default function save() {
+export default function save({ attributes }) {
+	const { adminOnly } = attributes;
 	const blockProps = useBlockProps.save();
 	return (
 		<div
 			{ ...blockProps }
 			className={ classNames(
 				blockProps.className,
-				'wpcloud-block-site-detail-card'
+				'wpcloud-block-site-detail-card',
+				{
+				 'wpcloud-admin-only': adminOnly,
+				}
 			) }
 		>
 			<InnerBlocks.Content />
