d9f595f115af30018b77008fe98c045996afea19
jhnstn <jhnstn@pm.me>
2024-08-14 21:24:16 -0400

    linting

--- plugin/blocks/src/site-domains/view.js
+++ plugin/blocks/src/site-domains/view.js
@@ -2,15 +2,16 @@
 	wpcloud.hooks.addAction(
 		'wpcloud_form_response_site_alias_add',
 		'site_alias_add',
-		( result ) => {
-			if ( ! result.success ) {
-				alert( result.message ); // eslint-disable-line no-alert, no-undef
+		(result) => {
+			if (!result.success) {
+				alert(result.message); // eslint-disable-line no-alert, no-undef
 				return;
 			}
 			const form = document.querySelector(
 				'.wpcloud-site-alias-add--expanding-section'
 			);
-			wpcloud.hooks.doAction( 'wpcloud_expanding_section_toggle', form );
+			wpcloud.hooks.doAction('wpcloud_expanding_section_toggle', form);
 		}
 	);
+
 } )( window.wpcloud );
