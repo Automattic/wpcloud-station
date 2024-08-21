fc9f870dadab6c3951de39b88759d79b5527bc8f
jhnstn <jhnstn@pm.me>
2024-08-21 12:27:15 -0400

    don't limit the site details block to site pages
    the block itself does not need a site

--- plugin/blocks/src/site-details/render.php
+++ plugin/blocks/src/site-details/render.php
@@ -5,11 +5,6 @@
  * @package wpcloud
  */
 
-if ( ! is_wpcloud_site_post() ) {
-	return;
-}
-
-/* Return early if the user is not an admin and the block is admin only */
 if ( $attributes['adminOnly'] && ! current_user_can( 'manage_options' ) ) {
 	return;
 }
