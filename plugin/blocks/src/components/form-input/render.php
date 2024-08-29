f81723671750ef2f32c3e882e7efdcd2d87664a5
jhnstn <jhnstn@pm.me>
2024-08-29 16:59:48 -0400

    make input renderer more general

--- plugin/blocks/src/components/form-input/render.php
+++ plugin/blocks/src/components/form-input/render.php
@@ -16,6 +16,8 @@ if ( $limit_to_admin && ! current_user_can( 'manage_options' ) ) {
 
 $name       = $attributes['name'] ?? 'any';
 $input_type = $attributes['type'] ?? 'text';
+error_log( $input_type );
+
 
 $allowed = apply_filters( 'wpcloud_block_form_allow_field_' . $name, true, $attributes, $block );
 
@@ -23,16 +25,27 @@ if ( ! $allowed ) {
 	return '';
 }
 
-$content = apply_filters( 'wpcloud_block_form_render_field_' . $name, $content, $attributes, $block );
-$content = apply_filters( 'wpcloud_block_form_render_field', $content, $attributes, $block );
+$attributes = apply_filters( 'wpcloud_block_form_field_attributes_' . $name, $attributes, $block );
+$content    = apply_filters( 'wpcloud_block_form_render_field_' . $name, $content, $attributes, $block );
+$content    = apply_filters( 'wpcloud_block_form_render_field', $content, $attributes, $block );
 
+// Check block data for values and options.
+$current_value = $attributes['value'] ?? '';
+$options       = array();
+$aliases       = array();
+
+if ( isset( $attributes['options'] ) ) {
+
+	foreach ( $attributes['options'] as $option ) {
+		$options[ $option['value'] ] = $option['label'];
+	}
+}
+
+// Check mutable options for values and options.
 $site_mutable_options = WPCloud_Site::get_mutable_options();
-
 if ( array_key_exists( $name, $site_mutable_options ) ) {
 	$current_value = WPCLOUD_Site::get_detail( get_the_ID(), $name );
-	if ( 'ssh_port' === $name ) {
-		error_log( 'WP Cloud: ' . $current_value );
-	}
+
 	if ( is_wp_error( $current_value ) ) {
 		error_log( 'WP Cloud: ' . $current_value->get_error_message() );
 		$current_value = '';
@@ -42,37 +55,43 @@ if ( array_key_exists( $name, $site_mutable_options ) ) {
 	}
 
 	if ( 'select' === $input_type ) {
-		$options      = $site_mutable_options[ $name ]['options'];
-		$aliases      = $site_mutable_options[ $name ]['option_aliases'] ?? array();
-		$options_html = '';
-		if ( ! is_wp_error( $options ) ) {
-			foreach ( $options as $value => $label ) {
+		$options = $site_mutable_options[ $name ]['options'] ?? $options;
+		$aliases = $site_mutable_options[ $name ]['option_aliases'] ?? $aliases;
+	}
+} else {
+	error_log( print_r( $options, true ) );
+}
 
-				$selected = selected( $current_value, $value, false );
-				// check for option aliases.
-				if ( ! $selected && array_key_exists( $value, $aliases ) ) {
-					$selected = selected( $current_value, $aliases[ $value ], false );
-				}
-				$options_html .= sprintf(
-					'<option value="%s" %s>%s</option>',
-					esc_attr( $value ),
-					esc_attr( $selected ),
-					esc_html( $label )
-				);
-			}
-		}
+if ( 'select' === $input_type ) {
+	$options_html = '';
+	if ( ! is_wp_error( $options ) ) {
+		foreach ( $options as $value => $label ) {
 
-		$regex   = '/(<select[^>]*>)(?:\s*<option[^>]*>.*?<\/option>)*\s*(<\/select>)/';
-		$content = preg_replace( $regex, '$1' . $options_html . '$2', $content );
-	} else {
-		$regex = '/(<input .*)\/>/';
-		if ( 'checkbox' === $input_type ) {
-			if ( $current_value ) {
-				$content = preg_replace( $regex, '$1 checked />', $content, 1 );
+			$selected = selected( $current_value, $value, false );
+			// check for option aliases.
+			if ( ! $selected && array_key_exists( $value, $aliases ) ) {
+				$selected = selected( $current_value, $aliases[ $value ], false );
 			}
-		} else {
-			$content = preg_replace( $regex, '$1 value="' . $current_value . '" />', $content, 1 );
+			$options_html .= sprintf(
+				'<option value="%s" %s>%s</option>',
+				esc_attr( $value ),
+				esc_attr( $selected ),
+				esc_html( $label )
+			);
 		}
 	}
+
+	$regex   = '/(<select[^>]*>)(?:\s*<option[^>]*>.*?<\/option>)*\s*(<\/select>)/';
+	$content = preg_replace( $regex, '$1' . $options_html . '$2', $content );
+} else {
+	$regex = '/(<input .*)\/>/';
+	if ( 'checkbox' === $input_type ) {
+		if ( $current_value ) {
+			$content = preg_replace( $regex, '$1 checked />', $content, 1 );
+		}
+	} else {
+		$content = preg_replace( $regex, '$1 value="' . $current_value . '" />', $content, 1 );
+	}
 }
+
 echo $content; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
