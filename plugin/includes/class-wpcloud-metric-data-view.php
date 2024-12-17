dac69b1d15d2edc1f93aafbc7e7b9bbbeaee376e
jhnstn <jhnstn@pm.me>
2024-12-17 18:11:05 -0500

    add metrics view class

---
+++ plugin/includes/class-wpcloud-metric-data-view.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * WP Cloud Metrics
+ *
+ * @package wpcloud
+ */
+
+declare( strict_types = 1 );
+
+require_once __DIR__ . '/class-wpcloud-metrics.php';
+
+/**
+ * WP Cloud Metric Data
+ */
+class WPCLOUD_Metric_Data_View extends WPCLOUD_Metrics {
+
+	/**
+	 * The map.
+	 *
+	 * @var array
+	 */
+	public $map;
+
+
+	/**
+	 * The data.
+	 *
+	 * @var array
+	 */
+	public $data;
+
+
+	/**
+	 * Get status codes
+	 *
+	 * @param bool $plot_view Whether to return a plot view.
+	 *
+	 * @return WP_Error|WPCLOUD_Metric_Data
+	 */
+	public function status_codes( $plot_view = true ): WP_Error|WPCLOUD_Metric_Data_View {
+		$this->requests( 'http_status' );
+
+		if ( ! $plot_view ) {
+			return $this;
+		}
+		if ( is_wp_error( $this->result ) ) {
+			return $this->result;
+		}
+
+		// First set up the x axis, noting how many status codes we encounter.
+		$status_codes = array();
+		$x            = array();
+		foreach ( $this->periods as $row ) {
+			$x[]          = $row['timestamp'];
+			$status_codes = array_unique( array_merge( $status_codes, array_keys( $row['dimension'] ) ) );
+		}
+		$this->data = array( $x );
+
+		$this->map = array(
+			'status_codes' => $status_codes,
+		);
+
+		// Set up y axis data.
+		$status_code_data = array();
+		foreach ( $status_codes as $status ) {
+			$status_code_data[ $status ] = array();
+		}
+
+		$resolution = $this->meta['resolution'];
+		foreach ( $this->periods as $row ) {
+			foreach ( $status_codes as $status ) {
+				$data_point = null;
+				if ( isset( $row['dimension'][ $status ] ) ) {
+					// Normalize the data to the resolution to get a total data point.
+					$data_point = $resolution * $row['dimension'][ $status ];
+				}
+				$status_code_data[ $status ][] = $data_point;
+			}
+		}
+		ksort( $status_code_data );
+		foreach ( $status_code_data as $status => $data ) {
+			$this->data[] = $data;
+		}
+
+		return $this;
+	}
+}
