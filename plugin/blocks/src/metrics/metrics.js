2237d8c5a43d2261a9ea67348de60f9efe0c90a7
jhnstn <jhnstn@pm.me>
2024-12-31 10:54:51 -0500

    fix interval prop

--- plugin/blocks/src/metrics/metrics.js
+++ plugin/blocks/src/metrics/metrics.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import Graph from '../components/graph/components/graph.js';
 
-function Metrics({ graphs, site }) {
+function Metrics({ graphs, site, interval }) {
 
 	const graphComponents = graphs.map((graph, index) => {
-		return <Graph key={index} {...graph} site={site} />;
+		return <Graph key={index} {...graph} site={site} interval={interval} />;
 	});
 	return (
 		<div className="wpcloud-metrics">
--- plugin/blocks/src/metrics/view.js
+++ plugin/blocks/src/metrics/view.js
@@ -4,10 +4,7 @@ import Metrics from './metrics.js';
 
 
 document.addEventListener('DOMContentLoaded', () => {
- /**
-	* @TODO fetch the site id from wpcloud.something....
-	*/
-	const siteId = 150743966;
+	const siteId = wpcloudSite?.id;
 	const container = document.getElementById('metrics');
 	const graphs = Array.from(container.querySelectorAll('.wp-block-wpcloud-graph')).map((graph) => {
 		const data = JSON.parse(graph.dataset.graphAttributes);
@@ -17,5 +14,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 	const root = createRoot(container);
-	root.render(<Metrics graphs={graphs} site={siteId}  />);
+	root.render(<Metrics graphs={graphs} site={siteId} interval={{ start: "1h" }} />);
 });
