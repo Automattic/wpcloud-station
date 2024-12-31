d98103a5ba871cb7f350ed5b102bbf86e43e28d1
jhnstn <jhnstn@pm.me>
2024-12-31 11:23:24 -0500

    clean up

--- plugin/blocks/src/components/graph/index.php
+++ plugin/blocks/src/components/graph/index.php
@@ -12,8 +12,6 @@ function enqueue_uplot_assets() {
 	wp_enqueue_style(
 		'uplot-css',
 		'https://unpkg.com/uplot/dist/uPlot.min.css',
-		array(),
-		null
 	);
 }
 add_action( 'enqueue_block_assets', 'enqueue_uplot_assets' );
