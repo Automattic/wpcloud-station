8fe57c7fb0dca6b30eefdfc3573f338493217b72
jhnstn <jhnstn@pm.me>
2025-01-17 13:55:03 -0500

    update card section to use heading controls

--- plugin/blocks/src/components/card-section/index.js
+++ plugin/blocks/src/components/card-section/index.js
@@ -6,7 +6,8 @@ import {
 	useBlockProps,
 	InspectorControls,
 	RichText,
-	HeadingLevelDropdown
+	HeadingLevelDropdown,
+	BlockControls
 } from '@wordpress/block-editor';
 import { PanelBody, SelectControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
@@ -22,29 +23,39 @@ import { useSyncMetaName } from '@wpcloud/hooks';
 
 const edit = ( { clientId, attributes, setAttributes } ) => {
 	const blockProps = useBlockProps();
-	const { tag, text, section } = attributes;
+	const { tag, text, section, level, levelOptions } = attributes;
+	const tagName = 'h' + level;
 
 	useSyncMetaName(clientId, text);
 
 	const controls = (
-	<InspectorControls>
-		<PanelBody label={__('Settings')}>
-			<SelectControl
-						label={__('Tag')}
-						value={tag}
-						options={[
-							{ label: 'span', value: 'span' },
-							{ label: 'h1', value: 'h1' },
-							{ label: 'h2', value: 'h2' },
-							{ label: 'h3', value: 'h3' },
-							{ label: 'h4', value: 'h4' },
-							{ label: 'h5', value: 'h5' },
-							{ label: 'h6', value: 'h6' },
-						]}
-						onChange={(value) => setAttributes({ tag: value} )}
-					/>
-		</PanelBody >
-	</InspectorControls>
+		<>
+			<BlockControls group="block">
+				<HeadingLevelDropdown
+					value={ level }
+					options={ levelOptions }
+					onChange={ (nextLevel) => setAttributes({ level: nextLevel }) }
+				/>
+			</BlockControls>
+			<InspectorControls>
+			<PanelBody label={__('Settings')}>
+				<SelectControl
+							label={__('Tag')}
+							value={tag}
+							options={[
+								{ label: 'span', value: 'span' },
+								{ label: 'h1', value: 'h1' },
+								{ label: 'h2', value: 'h2' },
+								{ label: 'h3', value: 'h3' },
+								{ label: 'h4', value: 'h4' },
+								{ label: 'h5', value: 'h5' },
+								{ label: 'h6', value: 'h6' },
+							]}
+							onChange={(value) => setAttributes({ tag: value} )}
+						/>
+			</PanelBody >
+		</InspectorControls>
+	</>
 	);
 
 	const Container = section === 'header' ? 'header' : 'footer';
@@ -53,7 +64,7 @@ const edit = ( { clientId, attributes, setAttributes } ) => {
 			{ controls }
 			<Container { ...blockProps }>
 				<RichText
-					tagName={tag}
+					tagName={tagName}
 					value={text}
 					onChange={(value) => setAttributes({ text: value })}
 					placeholder={ section === 'header' ? __( 'Header' ) : __( 'Footer' ) }
@@ -72,7 +83,6 @@ registerBlockType(metadata.name, {
 		if ( !text ) {
 			return null;
 		}
-		const blockProps = useBlockProps.save();
 		const richText = <RichText.Content tagName={tag} value={text} />
 		const Container = section === 'header' ? 'header' : 'footer';
 
