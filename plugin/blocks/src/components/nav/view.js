b4c288174ffaf75190a1379cb9caa510e8de50e7
jhnstn <jhnstn@pm.me>
2025-01-17 16:47:38 -0500

    fix current tab highlight

--- plugin/blocks/src/components/nav/view.js
+++ plugin/blocks/src/components/nav/view.js
@@ -1,7 +1,7 @@
 (() => {
 	const windowPath = window.location.pathname.replace(/\/$/, '');
 	document.querySelectorAll('.wp-block-wpcloud-nav a').forEach((link) => {
-		if (link.href?.includes(windowPath)) {
+		if (link.href?.endsWith(windowPath)) {
 			link.classList.add('contrast');
 			link.classList.add('current');
 			link.classList.remove('secondary');
