c10cf4b90b7698b0d26cd65e20f6db24de92ecaf
jhnstn <jhnstn@pm.me>
2025-01-28 15:12:03 -0500

    update spinner controls source

--- plugin/blocks/src/controls/index.js
+++ plugin/blocks/src/controls/index.js
@@ -2,3 +2,4 @@ export { updateAttribute } from './utils';
 
 export { default as IconControls } from './iconControls';
 export { default as ButtonControls } from './buttonControls';
+export { default as SpinnerControls } from '@wpcloud/components/spinner/controls';
