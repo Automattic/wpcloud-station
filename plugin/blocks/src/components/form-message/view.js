d0a3a4eaaa75db8ac7da8b9546b535c8a7b2cd8b
jhnstn <jhnstn@pm.me>
2025-01-31 16:06:23 -0500

    set up message to handle ajax and rest responses

--- plugin/blocks/src/components/form-message/view.js
+++ plugin/blocks/src/components/form-message/view.js
@@ -5,6 +5,10 @@ formMessages.forEach((formMessage) => {
 	const { wpcloudAction, messageType, messageTemplate } = formMessage.dataset;
 
 	function renderAndShowMessage(response) {
+		// handle both admin-ajax and REST API responses
+		const success = response.success || response.ok;
+		response = response.data || response;
+
 		const render = new Function('response', `return \`${messageTemplate}\`;`);
 		const p = formMessage.querySelector('p');
 
@@ -17,7 +21,6 @@ formMessages.forEach((formMessage) => {
 			}
 		}
 
-		const success = response.success;
 		if (messageType === 'success' && success) {
 			renderTemplate();
 			formMessage.classList.remove('display-none');
