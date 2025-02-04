6e56e6dfe8c781cbcedb5ab3de1cdb103645f9ad
jhnstn <jhnstn@pm.me>
2025-02-04 11:53:52 -0500

    Version bump to v1.0.0-beta.9

--- plugin/wpcloud-station.php
+++ plugin/wpcloud-station.php
@@ -7,7 +7,7 @@
  * Author URI:      https://wp.cloud/
  * Text Domain:     wpcloud
  * Domain Path:     /languages
- * Version:         v1.0.0-beta.8
+ * Version:         v1.0.0-beta.9
  *
  * @package        wpcloud-station
  */
