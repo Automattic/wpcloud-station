038d50742b77422778f4096d8f20b1c984df5f4f
jhnstn <jhnstn@pm.me>
2025-02-05 22:46:42 -0500

    add default start and end boundaries

--- plugin/blocks/src/metrics/view.js
+++ plugin/blocks/src/metrics/view.js
@@ -1,7 +1,5 @@
 import { createRoot } from 'react-dom/client';
-
-import Metrics from './metrics.js';
-
+import Metrics from './components/metrics.js';
 
 document.addEventListener('DOMContentLoaded', () => {
 	const siteId = wpcloudSite?.id;
@@ -13,5 +11,5 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 
 	const root = createRoot(container);
-	root.render(<Metrics graphs={graphs} site={siteId} interval={{ start: "1h" }} />);
+	root.render(<Metrics graphs={graphs} site={siteId} interval={{ start: "now-1h", end: "now" }} />);
 });
