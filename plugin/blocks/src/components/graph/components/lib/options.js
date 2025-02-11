8e9a204848804f489c194305fd6e1f7bd7abf830
jhnstn <jhnstn@pm.me>
2025-02-11 16:06:01 -0500

    refactor options builders

--- plugin/blocks/src/components/graph/components/lib/options.js
+++ plugin/blocks/src/components/graph/components/lib/options.js
@@ -6,57 +6,33 @@ import { stack } from './utils';
 const statusScale = chroma.scale( [ 'green', 'yellow', 'orange', 'red' ] ).domain( [ 200, 300, 400, 500 ] );
 const indexScale = chroma.scale( [ 'yellow', '008ae5'] ).domain( [ 0, 100 ] );
 
-
-export function addSeriesFill(series, idx) {
-	if ( isNaN( parseInt( series.label || '' ) ) ) {
-		series.fill = indexScale( idx ).hex();
-	} else {
-		series.fill = statusScale( series.label ).hex();
-	}
-	series.fillTo = () => 0;
-	return series;
+function buildSeries(series, options) {
+	const { fillScale = (s,idx) => indexScale(idx).hex(), fillTo = 0 } = options;
+	const ySeries = series.slice(1).map((s, idx) => {
+		s.fill = fillScale( s, idx );
+		s.fillTo = fillTo;
+		return s;
+	});
+	return [ series[0], ...ySeries ];
 }
 
-export function stackedOptions({ graphWidth, graphHeight, series, data:d, title, ...options }) {
-	console.log("stackedOptions", d);
+
+export function stackedOptions({ series, data:d, ...rest}) {
 	const { bands, data  } = stack(d);
+	const plugins = [seriesBarsPlugin({ stacked: true })];
+	const stackedSeries = buildSeries(series, { fillScale: (s) => statusScale(s.label).hex() });
+	return withDefaultOptions({ bands, data, plugins, series: stackedSeries, ...rest });
+}
 
-	const plugins = [ seriesBarsPlugin({ stacked: true}) ];
-
-	let opts = {
-		bands,
-		plugins,
-		title,
-		data,
-		width: graphWidth,
-		height: graphHeight,
-		series: series.map(addSeriesFill),
-		scales: {
-			y: {
-				range: [ 0, null ],
-				ori: 1
-			  }
-		},
-
-		axes: [
-			{
-			 // See the stacked series plugin for changing the x axis labels.
-			},
-			{
-				side: 3,
-			}
-		],
-		legend: {
-			live: false,
-			markers: {
-				width: 0,
-			}
-		},
-		padding: [ null, 0, null, 0 ],
-
+function withDefaultOptions( opts ) {
+	return {
+		padding: [null, 0, null, 0],
 		ori: 0,
-
+		axes: [{}, {}],
+		legend: { live: false, markers: { width: 0 } },
+		scales: {
+			y: { range: [0, null], ori: 1 }
+		},
+		...opts
 	};
-
-	return { ...opts, ...options };
 }
