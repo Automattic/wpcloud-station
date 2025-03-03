9ca8a70f392945559f86ddaa2f5b2daf0e18de48
jhnstn <jhnstn@pm.me>
2025-03-03 17:30:32 -0500

    add Jetpack SSO setup to client
    also fix path to plugin

--- plugin/hosting/client-station.php
+++ plugin/hosting/client-station.php
@@ -20,7 +20,7 @@ add_filter(
 		// Define the plugins you want to protect from being deactivated.
 		$protected_plugins = array(
 			'jetpack/jetpack.php',
-			'wpcloud-station/wpcloud-station.php', // Add your plugin paths here.
+			'wpcloud-station-plugin/wpcloud-station.php', // Add your plugin paths here.
 		);
 		if ( in_array( $plugin_file, $protected_plugins, true ) ) {
 			unset( $actions['deactivate'] );
@@ -53,4 +53,27 @@ add_action(
 			Jetpack::try_connection();
 		}
 	}
+);
+
+// Require Jetpack SSO & 2FA.
+add_filter( 'jetpack_remove_login_form', '__return_true' );
+add_filter( 'jetpack_sso_require_two_step', '__return_true' );
+add_filter( 'jetpack_sso_match_by_email', '__return_true' );
+
+// Prevent Jetpack & WP Cloud Station plugins from being deactivated.
+add_filter(
+	'plugin_action_links',
+	function ( $actions, $plugin_file ) {
+		// Define the plugins you want to protect from being deactivated.
+		$protected_plugins = array(
+			'jetpack/jetpack.php',
+			'wpcloud-station-plugin/wpcloud-station.php', // Add your plugin paths here.
+		);
+		if ( in_array( $plugin_file, $protected_plugins, true ) ) {
+			unset( $actions['deactivate'] );
+		}
+		return $actions;
+	},
+	10,
+	2
 );
