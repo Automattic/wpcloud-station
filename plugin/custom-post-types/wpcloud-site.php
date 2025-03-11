37066de7eb46bbc881896d116d8250ccd48e37e4
jhnstn <jhnstn@pm.me>
2025-03-11 10:30:08 -0400

    Import site if a non existent site is provisioned

--- plugin/custom-post-types/wpcloud-site.php
+++ plugin/custom-post-types/wpcloud-site.php
@@ -134,6 +134,14 @@ function wpcloud_lookup_post_by_site_id( int $wpcloud_site_id ): mixed {
 function wpcloud_on_site_provisioned( int $timestamp, int $wpcloud_site_id ): void {
 	$post = wpcloud_lookup_post_by_site_id( $wpcloud_site_id );
 	if ( ! $post ) {
+		// Check the webhook settings to see if we should create a site.
+		$wp_cloud_options = get_option( 'wp_cloud_options', array() );
+		$create_site      = $wp_cloud_options['proxy_webhook_create_site'] ?? false;
+		$owner            = $wp_cloud_options['proxy_webhook_default_owner'] ?? null;
+		if ( ! $create_site || ! $owner ) {
+			return;
+		}
+		WPCLOUD_Site::import( $wpcloud_site_id, $owner );
 		return;
 	}
 
