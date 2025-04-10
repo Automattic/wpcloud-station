9884d6777fd6f8bfcf7c336a7e5eb2eda08e3efe
jhnstn <jhnstn@pm.me>
2025-04-10 17:22:36 -0400

    skip horizontal line graphs for now

--- plugin/blocks/src/components/graph/components/lib/options.js
+++ plugin/blocks/src/components/graph/components/lib/options.js
@@ -37,13 +37,45 @@ function buildSeries(series, options = {}) {
 	return [series[0], ...ySeries];
 }
 
-function withDefaultOptions({ ori = 0, ...opts }) {
+function withDefaultOptions(opts) {
+	return {
+		padding: [null, 0, null, 0],
+		ori: 0,
+		axes: [{}, {}],
+		legend: { live: false, markers: { width: 2 } },
+		scales: {
+			y: {
+				range: [0, null],
+				ori: 1,
+			}
+		},
+		...opts
+	};
+}
+
+function withDefaultBarOptions({ ori = 0, ...opts }) {
 	// if horizontal ( ori === 1 ) place the y axis marks on the top (0)
 	// if vertical ( ori === 0 ) place the y axis marks on the right (3)
 	const side = ori === 0 ? 3 : 0;
 	// if horizontal ( ori === 1 ) set the y axis to read from top to bottom (1)
 	// if vertical ( ori === 0 ) set the y axis to read from left to right (0)
 	const yOri = ori === 0 ? 1 : 0;
+	const newOpts =  withDefaultOptions({
+		...opts,
+		ori,
+		axes: [{}, {
+			side
+		}],
+		scales: {
+			y: {
+				range: [0, null],
+				ori: yOri,
+			}
+		},
+	});
+	console.log('newOpts', newOpts);
+	return newOpts;
+	/*
 	return {
 		padding: [null, 0, null, 0],
 		ori,
@@ -59,6 +91,7 @@ function withDefaultOptions({ ori = 0, ...opts }) {
 		},
 		...opts
 	};
+	*/
 }
 
 function scaleFunc(options) {
@@ -72,27 +105,30 @@ export function stackedOptions({ series:s, data:d, useStatus, seriesOptions, dir
 	const plugins = [seriesBarsPlugin({ stacked: true, dir, ori })];
 	const fillScale = scaleFunc({ series: s, useStatus, seriesOptions });
 	const series = buildSeries(s, { fillScale, ...seriesOptions });
-	return withDefaultOptions({ bands, data, plugins, series, ori, ...options });
+	return withDefaultBarOptions({ bands, data, plugins, series, ...options });
 }
 
 export function barOptions({ series: s, useStatus, seriesOptions, dir, ori, ...options }) {
-	console.log('barOptions', dir, ori);
 	const plugins = [seriesBarsPlugin({ dir, ori })];
 	const fillScale = scaleFunc({ series: s, useStatus, seriesOptions });
 	const series = buildSeries(s, { fillScale, ...seriesOptions });
-	return withDefaultOptions({ series, plugins, ori, ...options });
+	return withDefaultBarOptions({ series, plugins, ...options });
 }
 
 export function lineOptions({ series, useStatus, seriesOptions, ...options }) {
+	// @TODO: Before allowing vertical line graphs, we need to figure out how to rotate the data.
+	const ori = 0;
 	const strokeScale = scaleFunc({ series, useStatus, seriesOptions });
-	return withDefaultOptions({ series: buildSeries(series, { strokeScale, ...seriesOptions }), ...options });
+	return withDefaultOptions({ series: buildSeries(series, { strokeScale, ...seriesOptions }), ori, ...options });
 }
 
 export function areaOptions({ series, useStatus, seriesOptions, ...options }) {
+	// @TODO: Before allowing vertical area graphs, we need to figure out how to rotate the data.
+	const ori = 0;
 	const scaleOptions = {series, useStatus, seriesOptions};
 	const fillScale = scaleFunc(scaleOptions);
 	const strokeScale = scaleFunc(scaleOptions);
-	return withDefaultOptions({ series: buildSeries(series, { fillScale, strokeScale, ...seriesOptions }), ...options });
+	return withDefaultOptions({ series: buildSeries(series, { fillScale, strokeScale, ...seriesOptions }), ori, ...options });
 }
 
 export function defaultOptions({ series, useStatus, seriesOptions, ...rest }) {
