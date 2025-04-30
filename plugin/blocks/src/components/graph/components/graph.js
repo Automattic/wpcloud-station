8b371ac0fc9ff9065a074e7720ccc2684ae33882
jhnstn <jhnstn@pm.me>
2025-04-29 23:34:51 -0400

    add metrics options context
    This allow for setting up default styles for each graph. Individual graphs will be able to override this in followup work

--- plugin/blocks/src/components/graph/components/graph.js
+++ plugin/blocks/src/components/graph/components/graph.js
@@ -18,7 +18,7 @@ import Overlay from './overlay';
 // import { stackedOptions, defaultOptions, barOptions, lineOptions, areaOptions } from './lib/options';
 import useGraphOptions from './lib/useGraphOptions';
 import stationApi from '@wpcloud/utils/api';
-import { useApiContext } from '@wpcloud/metrics/components/apiContext';
+import { useApiContext } from '@wpcloud/metrics/components/contexts';
 import Filters from './lib/filters';
 
 /**
