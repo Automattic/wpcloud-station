d1a1e45a13526378933dfc61a8216d8fdc62a385
Jason Johnston <jhnstn@users.noreply.github.com>
2025-05-09 12:08:08 -0400

    Theme: Update site templates (#367)
    * update theme templates
    
    * update site page template name pattern
    
    makes it easier to see all the site templates next to each other
    
    * touch up metrics pages
    
    * update perf dash to use grid
    
    * update block spacing

--- plugin/includes/class-wpcloud-station.php
+++ plugin/includes/class-wpcloud-station.php
@@ -57,7 +57,7 @@ class WPCloud_Station {
 			wpcloud_setup_categories();
 		}
 
-		// Create add site page.
+		// Create add site pages.
 		$core_pages = array(
 			'add-site'              => array(
 				'post_title'    => 'Add Site',
@@ -67,19 +67,32 @@ class WPCloud_Station {
 
 			'performance-dashboard' => array(
 				'post_title'    => 'Performance Dashboard',
-				'post_content'  => '<!-- wp:pattern {"slug":"wpcloud-station/performance-dashboard"} /-->',
+				'post_content'  => '<!-- wp:heading {"style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"0","right":"0"},"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
+<h2 class="wp-block-heading" style="margin-top:var(--wp--preset--spacing--20);margin-right:0;margin-bottom:var(--wp--preset--spacing--20);margin-left:0;padding-right:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">Performance Dashboard</h2>
+<!-- /wp:heading --><!-- wp:pattern {"slug":"wpcloud-station/performance-dashboard"} /-->',
 				'post_category' => array( $wpcloud_core_cat->term_id, get_category_by_slug( WPCLOUD_CATEGORY_PRIVATE )->term_id ),
+				'page_template' => 'page-wide.html',
 			),
 		);
-		$query      = new WP_Query(
+		$query          = new WP_Query(
 			array(
 				'cat'           => $wpcloud_core_cat->term_id,
 				'post_type'     => 'any',
 				'post_name__in' => array_keys( $core_pages ),
 			)
 		);
-		$post_names = array_map( fn( $post ) => $post->post_name, $query->get_posts() );
+		$existing_posts = $query->get_posts();
+		$post_names     = array_map( fn( $post ) => $post->post_name, $existing_posts );
 
+		// First, update any existing pages with new templates.
+		foreach ( $existing_posts as $post ) {
+			$page_name = $post->post_name;
+			if ( isset( $core_pages[ $page_name ]['page_template'] ) ) {
+				update_post_meta( $post->ID, '_wp_page_template', $core_pages[ $page_name ]['page_template'] );
+			}
+		}
+
+		// Then create any missing pages.
 		foreach ( $core_pages as $page_name => $args ) {
 			if ( ! in_array( $page_name, $post_names, true ) ) {
 				$args    = wp_parse_args(
@@ -93,9 +106,19 @@ class WPCloud_Station {
 				$page_id = wp_insert_post( $args );
 				if ( is_wp_error( $page_id ) ) {
 					$errors[] = $page_id;
+				} elseif ( isset( $args['page_template'] ) ) {
+					// Set page template if specified.
+					update_post_meta( $page_id, '_wp_page_template', $args['page_template'] );
 				}
 			}
 		}
+
+		// Configure permalinks.
+		global $wp_rewrite;
+		$permalink_structure = '/%postname%/';
+		update_option( 'permalink_structure', $permalink_structure );
+		$wp_rewrite->set_permalink_structure( $permalink_structure );
+		flush_rewrite_rules();
 
 		return $errors;
 	}
