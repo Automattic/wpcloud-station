9eb1afe74f085888f4bfd4cc2290e4423f28c6dd
Jason Johnston <jhnstn@users.noreply.github.com>
2025-05-12 13:38:13 -0400

    Metrics: Update the side legend options (#368)
    * hide other series when clicked
    
    * add click events to the side legend
    
    * add tooltip to truncated side legend itmes
    
    * remove empty plugin
    
    * add toogle/isolate setting to side legends
    
    * Update side legend to honor the graph dimensions
    
    * clean up

--- plugin/blocks/src/components/graph/components/lib/uplot-plugins/index.js
+++ plugin/blocks/src/components/graph/components/lib/uplot-plugins/index.js
@@ -6,4 +6,3 @@ export { legendLabelClickPlugin } from './legendLabelClickPlugin';
 export { isolateStackedPlugin } from './isolateStackedPlugin';
 export { preventLegendClickPlugin } from './preventLegendClickPlugin';
 export { seriesBarsPlugin } from './seriesBarsPlugin';
-export { tooltipPlugin } from './tooltipPlugin';
--- plugin/blocks/src/components/graph/components/uplot-graph.js
+++ plugin/blocks/src/components/graph/components/uplot-graph.js
@@ -1,16 +1,14 @@
 /**
  * External dependencies
  */
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import UplotReact from 'uplot-react';
 import 'uplot/dist/uPlot.min.css';
-import classnames from 'classnames';
 
 /**
  * Internal dependencies
  */
 import Overlay from './overlay';
-import SideLegend from './SideLegend';
 import useGraphOptions from './lib/useGraphOptions';
 import useUplotHeight from './lib/useUplotHeight';
 
@@ -18,12 +16,19 @@ import useUplotHeight from './lib/useUplotHeight';
  * UplotGraph component for rendering charts using uPlot
  *
  * @param {Object} props - Component props
+ * @param {Array} props.data - Chart data (controlled by state in graph.js)
+ * @param {boolean} props.refreshing - Whether the chart is refreshing (controlled by state in graph.js)
+ * @param {Object} props.options - All other options for the chart
  * @returns {JSX.Element} - Rendered component
  */
-
 export default function UplotGraph({
-		title,
 		data,
+		refreshing,
+		options
+}) {
+	// Extract options
+	const {
+		title,
 		series,
 		meta,
 		containerRef,
@@ -32,16 +37,25 @@ export default function UplotGraph({
 		type,
 		orientation,
 		dimension,
-		refreshing
-}) {
+	} = options;
 	// Use useRef instead of useState to avoid re-renders when setting the uPlot instance
 	const uplotInstanceRef = useRef(null);
+	// Store the original data
+	const originalDataRef = useRef(data);
+	// Store the current chart data
+	const [chartData, setChartData] = useState(data);
+
+	// Update original data reference when data changes
+	useEffect(() => {
+		originalDataRef.current = data;
+		setChartData(data);
+	}, [data]);
 
 	// Use the height adjustment hook
-	const { containerDimensionsRef, handleChartCreated } = useUplotHeight({
+	const { handleChartCreated } = useUplotHeight({
 		containerRef,
 		uplotInstanceRef,
-		data,
+		data: chartData,
 		series,
 		showLegend
 	});
@@ -49,7 +63,7 @@ export default function UplotGraph({
 	// Memoize the options params to avoid unnecessary re-renders
 	const graphOptionsParams = React.useMemo(() => ({
 		title,
-		data,
+		data: chartData,
 		series,
 		meta,
 		containerRef,
@@ -58,21 +72,20 @@ export default function UplotGraph({
 		type,
 		orientation,
 		dimension
-	}), [title, data, series, meta, containerRef, showLegend, legendPosition, type, orientation, dimension]);
+	}), [title, chartData, series, meta, containerRef, showLegend, legendPosition, type, orientation, dimension]);
 
 	// Get graph options from the hook
-	const { data: d, ...options } = useGraphOptions(graphOptionsParams);
+	// Use chartData instead of the original data
+	const { data: d, ...uplotOptions } = useGraphOptions(graphOptionsParams);
 
-	// Extract colors from series for the SideLegend
-	const seriesColors = options.series.map(s => s.stroke).filter(Boolean);
+	// No need to extract colors here as they're handled in the SideLegend component
 
+	// Return the chart
 	return (
-		<div className={classnames('wpcloud-uplot-graph',{
-			'wpcloud-uplot-graph--with-side-legend': showLegend && legendPosition === 'right'
-		})}>
-			<div className="wpcloud-uplot-graph__chart-container">
+		<div className="wpcloud-uplot-graph">
+			<div className="wpcloud-uplot-graph__chart-container" style={{ height: '100%' }}>
 				<UplotReact
-					options={options}
+					options={uplotOptions}
 					data={d}
 					onCreate={(chart) => {
 						// Only set the instance if it's not already set or if it's a different instance
@@ -86,15 +99,6 @@ export default function UplotGraph({
 				/>
 				{refreshing && <Overlay refreshing={true} />}
 			</div>
-
-			{showLegend && legendPosition === 'right' && (
-				<div className="wpcloud-uplot-graph__legend-container">
-					<SideLegend
-						series={options.series}
-						colors={seriesColors}
-					/>
-				</div>
-			)}
 		</div>
 	);
 }
